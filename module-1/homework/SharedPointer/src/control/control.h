48e175b0e1e742cd9e153b8eb4db3d3081b203db
aidenne <aiuvarova_1@edu.hse.ru>
2021-06-10 15:27:38 +0300

    style

--- module-1/homework/SharedPointer/src/control/control.h
+++ module-1/homework/SharedPointer/src/control/control.h
@@ -4,10 +4,9 @@
 
 class SharedCount {
 public:
-
     SharedCount() = default;
 
-    explicit SharedCount(size_t refs) : strongRefCount(refs) {};
+    explicit SharedCount(size_t refs) : strongRefCount(refs){};
 
     virtual ~SharedCount() = default;
 
@@ -25,7 +24,6 @@ public:
 
 protected:
     std::atomic<size_t> strongRefCount{0};
-
 };
 
 class SharedWeakCount : public SharedCount {
@@ -48,17 +46,16 @@ public:
 
 protected:
     std::atomic<size_t> weakRefCount{0};
-
 };
 
-template<typename T, typename Deleter=std::default_delete<T*>>
+template <typename T, typename Deleter = std::default_delete<T *>>
 class ControlBlock : public SharedWeakCount {
 public:
     ControlBlock() = default;
 
-    explicit ControlBlock(T& obj) : managed_object_(obj) {};
+    explicit ControlBlock(T &obj) : managed_object_(obj){};
 
-    ControlBlock(T& obj, Deleter& d) : managed_object_(obj), deleter_(d) {};
+    ControlBlock(T &obj, Deleter &d) : managed_object_(obj), deleter_(d){};
 
 private:
     T managed_object_;
